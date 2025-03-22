93fca025ed71d22650b0d3d7dbb7d3909a7ba10d
LardexTheLarge <0gabevee0@gmail.com>
2025-03-21 18:28:15 -0600

    initial commit for text_editor app

---
+++ utils/Text_editor.py
@@ -0,0 +1,5 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox, font
+from tkinter.scrolledtext import ScrolledText
+from utils.theme_manager_classes import ThemeManager  # Integrate with existing theme system
+from utils.message_popup import MessagePopup
